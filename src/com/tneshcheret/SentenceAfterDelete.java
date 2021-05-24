31d1ed099ac0b6f3356ea3b84a4691d7cd12e58d
Tetiana Neshcheret <neshcherett10@gmail.com>
2021-05-24 23:22:36 +0300

    Improved classes SentenceAfterDelete. Version 3

--- src/com/tneshcheret/SentenceAfterDelete.java
+++ src/com/tneshcheret/SentenceAfterDelete.java
@@ -5,8 +5,24 @@ public class SentenceAfterDelete implements StringWorker {
 
     @Override
     public int execute(String sentence, String word) {
-        String s = " " + word;
-        String newSentence = sentence.replaceAll(word, "");
-        return newSentence.length();
+        if (sentence.contains(" " + word + " ") || sentence.contains(" " + word) || sentence.contains(word + " ")) {
+            String[] partsSentence = sentence.split("\\s+");
+            for (int i = 0; i < partsSentence.length; i++) {
+                if (partsSentence[i].equalsIgnoreCase(word)) {
+                    partsSentence[i] = "";
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < partsSentence.length; i++) {
+                if (i == partsSentence.length - 1) {
+                    stringBuilder.append(partsSentence[i]);
+                } else {
+                    stringBuilder.append(partsSentence[i]).append(" ");
+                }
+            }
+            System.out.println(stringBuilder);
+            return stringBuilder.length();
+        }
+        return sentence.length();
     }
 }
